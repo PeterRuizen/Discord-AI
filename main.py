65ad263333e00e02609ae5d2c823800d4ce12c5b
PeterRuizen <peter.ruizendaal@student.hu.nl>
2024-08-11 00:00:15 +0200

    added some simple test commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import settings
 import discord
 from discord.ext import commands
@@ -6,12 +7,36 @@ logger = settings.logging.getLogger("bot")
 
 def run():
     intents = discord.Intents.default()
+    intents.message_content = True
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    bot = commands.Bot(command_prefix="?", intents=intents)
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.command(
+            help="This is help in full so how the ping command will be displayed",
+            brief="The brief version of the help you'll see",
+            description="The description, here follows the description of the command",
+            enable=True,
+            hidden=False
+    )
+    async def ping(ctx):
+        """Ping the bot"""
+        await ctx.send("Pong!")
+
+    @bot.command()
+    async def choice(ctx, *options):
+        await ctx.send(random.choice(options))
+
+    @bot.command(
+            help="repeats what you say with a space in between, only two words",
+            brief="repeats what you say",
+            description="here follows the description of the command",
+    )
+    async def say(ctx, what = "WHAT?", why = "WHY?"):
+        await ctx.send(what + " " + why)
 
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
