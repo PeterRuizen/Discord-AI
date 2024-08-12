1dbd30c68304e15fee652794918f43fff1ed482e
PeterRuizen <peter.ruizendaal@student.hu.nl>
2024-08-13 01:06:31 +0200

    added cogs for better command orginization, added auth check for commands, added reload to reload the bot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import settings
 import discord
+import cogs.greetings
 from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
+
+async def is_owner(ctx):
+    return ctx.author.id == ctx.guild.owner_id
 
 def run():
     intents = discord.Intents.default()
@@ -17,6 +21,23 @@ def run():
         for cmd_file in settings.CMDS_DIR.glob("*.py"):
             if cmd_file.name != "__init__.py":
                 await bot.load_extension(f"cmds.{cmd_file.name[:-3]}")
+
+        for cog_file in settings.COGS_DIR.glob("*.py"):
+            if cog_file.name != "__init__.py":
+                await bot.load_extension(f"cogs.{cog_file.name[:-3]}")
+
+    @bot.command()
+    async def reload(ctx, cog: str):
+        await bot.reload_extension(f"cogs.{cog.lower()}")
+
+    @bot.command()
+    async def load(ctx, cog: str):
+        await bot.load_extension(f"cogs.{cog.lower()}")
+
+    @bot.command()
+    @commands.check(is_owner)
+    async def unload(ctx, cog: str):
+        await bot.unload_extension(f"cogs.{cog.lower()}")    
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
